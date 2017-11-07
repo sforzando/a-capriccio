4c3801117f665cf8a9386b26e515ed87a95ddfcf
Shin'ichiro SUZUKI <shin@sforzando.co.jp>
2017-11-07 15:54:49 +0900

    v.1

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class CameraController : MonoBehaviour {
+
+	public Camera MainCamera;
+	public VideoPlayer Player;
+
+	private Vector3 lastMousePosition;
+	private Vector3 newAngle = new Vector3(0, 0, 0);
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Video Player
+		if ((ulong)Player.frame == Player.frameCount) {
+			SceneManager.LoadScene("Menu");
+		}
+
+		// Mouse
+		if (Input.GetMouseButtonDown(0)) {
+			newAngle = MainCamera.transform.localEulerAngles;
+			lastMousePosition = Input.mousePosition;
+		} else if (Input.GetMouseButton(0)) {
+			newAngle.x -= (Input.mousePosition.y - lastMousePosition.y) * 0.1f;
+			newAngle.y += (Input.mousePosition.x - lastMousePosition.x) * 0.1f;
+			MainCamera.gameObject.transform.localEulerAngles = newAngle;
+			lastMousePosition = Input.mousePosition;
+		}
+
+		// Keyboard
+		if (Input.anyKey) {
+			if (Input.GetKeyDown(KeyCode.Space)) {
+				if (Player.isPlaying) {
+					Player.Pause();
+				} else {
+					Player.Play();
+				}
+			} else if (Input.GetKeyDown(KeyCode.Escape)) {
+				SceneManager.LoadScene("Menu");
+			}
+
+			newAngle = MainCamera.transform.localEulerAngles;
+			if (Input.GetKey(KeyCode.UpArrow)) {
+				newAngle.x -= 5;
+			} else if (Input.GetKey(KeyCode.DownArrow)) {
+				newAngle.x += 5;
+			} else if (Input.GetKey(KeyCode.LeftArrow)) {
+				newAngle.y -= 10;
+			} else if (Input.GetKey(KeyCode.RightArrow)) {
+				newAngle.y += 10;
+			}
+			MainCamera.gameObject.transform.localEulerAngles = newAngle;
+		}
+	}
+}
---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+	public AudioSource SE;
+	public AudioClip SE_OK;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			Application.Quit();
+		} else if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+			onButton_Opening();
+		} else if (Input.GetKeyDown(KeyCode.RightArrow)) {
+			onButton_MavicPro();
+		}
+	}
+
+	public void onButton_Opening() {
+		SE.PlayOneShot(SE_OK);
+		StartCoroutine(Checking(()=>{
+			SceneManager.LoadScene("Main_Opening4K");
+		}));
+	}
+
+	public void onButton_MavicPro() {
+		SE.PlayOneShot(SE_OK);
+		StartCoroutine(Checking(()=>{
+			SceneManager.LoadScene("Main_MavicPro");
+		}));
+	}
+
+	public void onButton_Quit() {
+		Application.Quit();
+	}
+
+	public delegate void FunctionType();
+	private IEnumerator Checking(FunctionType callback){
+		while(true) {
+			yield return new WaitForFixedUpdate ();
+			if (!SE.isPlaying) {
+				callback();
+				break;
+			}
+		}
+	}
+}
